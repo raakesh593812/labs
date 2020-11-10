7a1a207b0a42f3843c885a143a0e24d1a8e472d6
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2020-11-10 21:10:52 +0530

    Create extension.ps1

---
+++ extension.ps1
@@ -0,0 +1,21 @@
+get-azvm | foreach-object {
+ Get-AzVMExtension -ResourceGroupName $_.ResourceGroupName -VMName $_.name | select vmname,name ,ResourceGroupName | foreach-object {
+if ($_.name -eq 'MicrosoftMonitoringAgent'){
+write-host "its windows" -f yellow
+$pubse = Get-AzVMExtension -ResourceGroupName $_.ResourceGroupName -VMName $_.vmname -name MicrosoftMonitoringAgent -verbose | select PublicSettings
+ $law = Get-AzOperationalInsightsWorkspace | Where-Object {$_.CustomerId -eq (($pubse.PublicSettings | convertfrom-json).workspaceId) } | select Name
+write-host $_.vmname $_.ResourceGroupName $law.name
+}
+elseif ($_.name -eq 'OMSAgentForLinux'){
+write-host "its linux" -f yellow
+$pubse = Get-AzVMExtension -ResourceGroupName $_.ResourceGroupName -VMName $_.vmname -name OMSAgentForLinux -verbose | select PublicSettings
+ $law = Get-AzOperationalInsightsWorkspace | Where-Object {$_.CustomerId -eq (($pubse.PublicSettings | convertfrom-json).workspaceId) } | select Name
+write-host $_.vmname $_.ResourceGroupName $law.name
+}
+
+
+
+ }
+
+
+}
